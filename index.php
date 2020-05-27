7a0e011147a218d0f1c3236a14972db80659e8fb
TMShader <43723765+TMShader@users.noreply.github.com>
2020-05-27 18:51:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <?php
 
 if (isset($_REQUEST['get_command']) and empty($_REQUEST['name']) == false) {
-    $command = escapeshellcmd('py ./getTellraw.py ' . $_REQUEST['name']);
+    $command = escapeshellcmd('python ./getTellraw.py ' . $_REQUEST['name']);
     $output = shell_exec($command);
     print_r('<p class="command">' . $output . '</p>');
     return;
