c9830ae595b040aa5f3219e1ffcc3f4e6fb8fa9e
TMShader <43723765+TMShader@users.noreply.github.com>
2020-05-27 20:07:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 if (isset($_REQUEST['get_command']) and empty($_REQUEST['name']) == false) {
     $command = escapeshellcmd('python ./getTellraw.py ' . $_REQUEST['name']);
     $output = shell_exec($command);
-    print_r('<p class="command">Command copied to clipboard!</p>');
+    print_r('<p class="command">Command copied to clipboard!' . $output . '</p>');
     print_r('<script type="text/javascript">copy("' . $output . '");</script>');
     return;
 }
